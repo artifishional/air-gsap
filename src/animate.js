2d62bea1855db3449cd5262e0ed067abf6aafb15
Полицинский Артем Сергеевич <artifishional@mail.ru>
2019-02-21 19:05:41 +0300

    - key frames - debug

--- src/animate.js
+++ src/animate.js
@@ -7,50 +7,50 @@ import {Howler} from "howler"
 const {performance} = window;
 
 function setprops(
-    node, { muted = null, argv = null, class: _class, attribute, order, style, sound, ...props } = {},
-    { resources, targeting = [] } = {},
+    node, {muted = null, argv = null, class: _class, attribute, order, style, sound, ...props} = {},
+    {resources, targeting = []} = {},
     intl,
     view = null
 ) {
 
-    if(order === "tofront" && node.parentNode) {
+    if (order === "tofront" && node.parentNode) {
         node.parentNode.append(node);
     }
 
-    if(_class) {
+    if (_class) {
         _class
             .split(" ")
             .filter(Boolean)
-            .map( cls => ({ cls: cls[0] === "!" ? cls.substr(1) : cls, toggle: cls[0] !== "!" }) )
-            .map( ({ cls, toggle }) => node.classList.toggle(cls, toggle));
+            .map(cls => ({cls: cls[0] === "!" ? cls.substr(1) : cls, toggle: cls[0] !== "!"}))
+            .map(({cls, toggle}) => node.classList.toggle(cls, toggle));
     }
 
-    if(muted !== null) {
+    if (muted !== null) {
         Howler.mute(muted);
     }
 
-    if(sound) {
-        const { sound: _sound } = resources.find( ({ type, name }) => name === sound && type === "sound") || {};
+    if (sound) {
+        const {sound: _sound} = resources.find(({type, name}) => name === sound && type === "sound") || {};
         _sound && _sound.play();
     }
 
     view && view.setprops(argv, intl);
 
-    if(attribute) {
-        for(let key in attribute) {
+    if (attribute) {
+        for (let key in attribute) {
             node.setAttribute(key, attribute[key]);
         }
     }
-    if(style) {
-        for(let key in style) {
+    if (style) {
+        for (let key in style) {
             node.style[key] = style[key];
         }
     }
     return props;
 }
 
-export default (view, frames, key) =>
-    stream((emt, {sweep, hook}) => {
+export default (view, keyframes, key) =>
+    stream((emt, { sweep, hook }) => {
 
         emt.kf();
 
@@ -58,24 +58,34 @@ export default (view, frames, key) =>
         //let _schema = new Schema(frames);
 
         sweep.add(() => _cache.map(([_, tl]) => tl.kill()));
-        hook.add(({ action: schema, intl = null, env: {time = performance.now(), ttmp = time, state = "play"} = {} }) => {
+        hook.add(({
+                      data: [ data, action ],
+                      env: { time = performance.now(), ttmp = time, state = "play" } = {}
+                  } = {}) => {
 
-            intl && view.setprops( null, intl );
+            //intl && view.setprops(null, intl);
+            //if (!Array.isArray(schema)) return;
+            //const inSchema = new Schema(frames).find(schema[0]);
 
-            if(!Array.isArray(schema)) return;
+            const schema = keyframes.find( ([ name ]) => action === name );
 
-            const inSchema = new Schema(frames).find(schema[0]);
+            if(schema) {
+
+                debugger;
+
+            }
+
+
 
             if (inSchema) {
 
                 let data = null;
 
-                if(inSchema.filler) {
-                    data = inSchema.fill( schema ).toJSON();
-                }
-                else {
-                    data = inSchema.merge( schema ).toJSON();
-                }
+                /*if (inSchema.filler) {
+                    data = inSchema.fill(schema).toJSON();
+                } else {
+                    data = inSchema.merge(schema).toJSON();
+                }*/
 
                 const [name, {
                     duration = -1,
@@ -85,11 +95,11 @@ export default (view, frames, key) =>
                     ...gprops
                 }, ...keys] = data;
 
-                log && console.log( ttmp, key, [ name, {duration, ...gprops }, ...keys ] );
+                log && console.log(ttmp, key, [name, {duration, ...gprops}, ...keys]);
                 const from = (time - ttmp) / 1000 - delay;
                 const gr = view.query(query);
 
-                if(!gr) return emt({action: `${schema[0]}-complete`});
+                if (!gr) return emt({action: `${schema[0]}-complete`});
 
                 /*const existIndex = _cache.findIndex(([x]) => name === x);
                 if (existIndex > -1) {
@@ -100,18 +110,16 @@ export default (view, frames, key) =>
                 _cache.map(([_, tl]) => tl.kill());
                 _cache.length = 0;
 
-                if(duration === -1) {
+                if (duration === -1) {
                     if (state === "play") {
-                        if(from < 0) {
-                            const tl = TweenMax.delayedCall(from < 0 ? -from : 0, () => setprops( gr, gprops, view.props, intl, view ));
+                        if (from < 0) {
+                            const tl = TweenMax.delayedCall(from < 0 ? -from : 0, () => setprops(gr, gprops, view.props, intl, view));
                             _cache.push([name, tl]);
-                        }
-                        else {
-                            setprops( gr, gprops, view.props, intl, view );
+                        } else {
+                            setprops(gr, gprops, view.props, intl, view);
                         }
                     }
-                }
-                else {
+                } else {
 
                     if (state === "play") {
                         const tl = new TimelineMax({
@@ -121,33 +129,33 @@ export default (view, frames, key) =>
                                 .map(([to, props], i, arr) => [to - (arr[i - 1] ? arr[i - 1][0] : 0), props])
                                 .map(([range, props]) => {
                                     const dur = range ? duration * range : 1e-10;
-                                    const {ease = "Power1.easeOut", ...cutprops} = setprops( document.createElement("div"), { ...gprops, ...props }, view.props, intl );
+                                    const {ease = "Power1.easeOut", ...cutprops} = setprops(document.createElement("div"), {...gprops, ...props}, view.props, intl);
 
                                     const res = [];
 
                                     const set = {
                                         ease: parseEase(ease),
                                         ...cutprops,
-                                        onComplete: () => setprops( gr, { ...gprops, ...props }, view.props, intl, view )
+                                        onComplete: () => setprops(gr, {...gprops, ...props}, view.props, intl, view)
                                     };
 
-                                    if(Object.keys(cutprops).length) {
-                                        res.push( new TweenMax(gr, dur, {
+                                    if (Object.keys(cutprops).length) {
+                                        res.push(new TweenMax(gr, dur, {
                                             ...set,
                                             ...cutprops
-                                        }) );
+                                        }));
                                     }
 
                                     //todo now only ___default___ argv supported
-                                    if(gprops.argv !== undefined) {
-                                        gprops.argv = { ___default___: gprops.argv };
+                                    if (gprops.argv !== undefined) {
+                                        gprops.argv = {___default___: gprops.argv};
                                         const argv = view.getargv();
-                                        res.push( new TweenMax(argv, dur, {
+                                        res.push(new TweenMax(argv, dur, {
                                             ...set, ...gprops.argv,
                                             onUpdate: () => {
                                                 view.setprops(argv, intl);
                                             }
-                                        }) );
+                                        }));
                                     }
 
                                     return res;
@@ -159,8 +167,7 @@ export default (view, frames, key) =>
                         _cache.push([name, tl]);
                     }
                 }
-            }
-            else {
+            } else {
                 //todo need timer
                 return emt({action: `${schema[0]}-complete`});
             }
@@ -168,10 +175,10 @@ export default (view, frames, key) =>
         });
     });
 
-    function parseEase(string) {
-        const easing = string.split(".");
-        if (easing.length === 2) return Ease[easing[0]][easing[1]];
-        const cfgExp = /true|false|(-?\d*\.?\d*(?:e[\-+]?\d+)?)[0-9]/ig;
-        const config = string.match(cfgExp).map(JSON.parse);
-        return Ease[easing[0]][easing[1]].config.apply(null, config);
-    }
+function parseEase(string) {
+    const easing = string.split(".");
+    if (easing.length === 2) return Ease[easing[0]][easing[1]];
+    const cfgExp = /true|false|(-?\d*\.?\d*(?:e[\-+]?\d+)?)[0-9]/ig;
+    const config = string.match(cfgExp).map(JSON.parse);
+    return Ease[easing[0]][easing[1]].config.apply(null, config);
+}
